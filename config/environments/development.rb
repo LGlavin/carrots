672ab1804ddd910d7b2ffc4182d853be5324319b
Elizabeth Glavin <eglavin@gmail.com>
2014-04-28 19:36:22 -0400

    styling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ JarBasic::Application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Do not eager load code on boot.
-  config.eager_load = false
+  config.eager_load = true
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
