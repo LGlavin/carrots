949ba80b9dbc6f89dbd97741a421b7d9aa9cd7a4
Elizabeth Glavin <eglavin@gmail.com>
2014-04-28 17:48:39 -0400

    precompiled assets

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,10 @@ Bundler.require(*Rails.groups)
 
 module JarBasic
   class Application < Rails::Application
+     config.assets.precompile += [
+      'welcome.css.scss'
+    ]
+    config.assets.precompile += %w( vendor/custom.modernizr )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
