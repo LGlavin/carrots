cfca4619b0b2356d8119af110e2400f64953d869
Elizabeth Glavin <eglavin@gmail.com>
2014-04-29 11:40:46 -0400

    product unit tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,11 @@
 class Product < ActiveRecord::Base
-  validates :title, :description, :image_url, presence: true
-  validates_length_of :title, :minimum => 10, :message => "at least 10 characters"
+  validates :name, :description, :image_url, presence: true
+  validates_length_of :name, :minimum => 10, :message => "at least 10 characters"
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, uniqueness: true
+  validates :name, uniqueness: true
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be URL for GIF, JPG or PNG image.'
   }
-  validates :title, length: {minimum: 10}
+  validates :name, length: {minimum: 10}
 end
